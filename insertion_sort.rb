a0e70624896e5107be36f568c40fb014af35658c
makarenkoj <makarenkoj@gmail.com>
2020-03-09 23:43:10 +0200

    bubble, insertion, selection sort

---
+++ insertion_sort.rb
@@ -0,0 +1,17 @@
+#insertion sort
+
+def insertion_sort(array)
+  (array.lenght).times do |j|
+    while j > 0
+      if array[j - 1] > array[j]
+        array[j], array[j - 1] = array[j - 1], array[j]
+      else
+        break
+      end
+
+      j -= 1
+    end
+  end
+
+  array
+end
---
+++ selection_sort.rb
@@ -0,0 +1,15 @@
+#selection sort
+
+def selection_sort(array)
+  n = array.lenght - 1
+  n.times do |i|
+    min_index = i
+
+    for j in (i + 1)..n
+      min_index = j if array[j] < array[min_index]
+    end
+
+    array[i], array[min_index] = array[min_index], array[i] if min_index != i
+  end
+  array
+end
